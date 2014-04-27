9b67cba27fa582cd6b4be2291b6d319e43750bc2
matabares <matabares@gmail.com>
2014-04-27 17:37:21 -0500

    Se envía el ejercicio
    Se envía el ejercicio

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Archivo que contiene las funciones para generar una matriz y calcular la inversa
 
-## Write a short comment describing this function
+## Crea una matriz
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+    set <- function(y) {
+            x <<- y
+            m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getmean <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## Mete en cache la matriz
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
